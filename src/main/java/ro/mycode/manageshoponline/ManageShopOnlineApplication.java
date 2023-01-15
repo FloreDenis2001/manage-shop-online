2cd54a28efc7e1f6292f0a978c96e43e822533d5
FloreDenis2001 <floredenis907@yahoo.com>
2023-01-15 06:36:10 +0200

    master

---
+++ src/main/java/ro/mycode/manageshoponline/ManageShopOnlineApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.manageshoponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageShopOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageShopOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/manageshoponline/ManageShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.manageshoponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageShopOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
